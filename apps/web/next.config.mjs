632228c4cb0f676206e3c9c7ca9cd2ecd184f0f8
awaaate <awate@awate>
2023-09-03 11:36:21 +0200

    att output tracing

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -29,7 +29,16 @@ const nextConfig = {
   },
   experimental: {
     serverActions: true,
+    outputFileTracingIgnores: [
+      "./**/node_modules/@swc/core-linux-x64-gnu",
+      "./**/node_modules/@swc/core-linux-x64-musl",
+      "./**/node_modules/esbuild/linux",
+      "./**/node_modules/webpack",
+      "./**/node_modules/rollup",
+      "./**/node_modules/terser",
+    ],
   },
+  outputFileTracing: true,
   reactStrictMode: false,
 
   typescript: {
