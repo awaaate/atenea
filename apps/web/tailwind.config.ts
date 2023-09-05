100c3da5d361c138f9c83c76c9e62536c90e30d0
awaaate <awate@awate>
2023-09-05 21:03:07 +0200

    fixed tailwind bundler paths in the web app

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -7,9 +7,10 @@ const config = {
         "./app/**/*.{ts,tsx}",
         "./components/**/*.{ts,tsx}",
         "./node_modules/@shared/ui/src/**/*.{ts,tsx}",
-        "./node_modules/@shared/editor/src/**/*.{ts,tsx}",
         "./node_modules/@shared/templates/src/**/*.{ts,tsx}",
-        "./node_modules/@shared/editor/node_modules/@tremor/**/*.{js,ts,jsx,tsx}"
+        "./node_modules/@shared/editor/src/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/@shared/editor/node_modules/@shared/views/src/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/@shared/editor/node_modules/@shared/views/node_modules/@tremor/**/*.{js,ts,jsx,tsx}"
     ],
     presets: [tremorPreset],
 } satisfies Config
