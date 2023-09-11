fda023fe398fa2ee0d658098ed3b8ef39417b1d1
awaaate <awate@awate>
2023-09-11 23:01:19 +0200

    without pnplock

--- packages/api/src/api/data-source/data-source.test.ts
+++ packages/api/src/api/data-source/data-source.test.ts
@@ -2,10 +2,11 @@ import { it } from "vitest"
 import { dataSourceRouter } from "./data-source"
 import { db } from "@shared/db"
 
-const caller = dataSourceRouter.createCaller({ user: null, })
+const caller = dataSourceRouter.createCaller({ user: null })
 
-it("Should get the proposal meta", async () => {
+it.skip("Should get the proposal meta", async () => {
     const data = await caller.proposalsMeta({ first: 20, orderBy: "createdTimestamp", orderDirection: "desc" })
     console.log(data)
     return data
 })
+
