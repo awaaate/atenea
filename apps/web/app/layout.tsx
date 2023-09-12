db21b02eae653e76cd80276bead6c2a14e86217f
awaaate <awate@awate>
2023-09-12 10:07:28 +0200

    preparing the knw board 2

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Inter } from "next/font/google";
+import { Londrina_Solid } from "next/font/google";
 import { Metadata } from "next";
 import { env } from "@/env.mjs";
 
@@ -8,7 +8,10 @@ import { siteConfig } from "@/config";
 import { MainProvider } from "@/components/providers/main-provider";
 import { TRPCProvider } from "@/components/providers/trpc-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const londrina = Londrina_Solid({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "900"],
+});
 
 export const metadata: Metadata = {
   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
@@ -30,7 +33,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={londrina.className}>
         <TRPCProvider>
           <MainProvider>{children}</MainProvider>
         </TRPCProvider>
