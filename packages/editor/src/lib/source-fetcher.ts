026f1923862d35763607a96ebd1e6258b54182a1
awaaate <awate@awate>
2023-09-25 23:11:50 +0200

    Updated the api endpoint to use the env variable

--- packages/editor/src/lib/source-fetcher.ts
+++ packages/editor/src/lib/source-fetcher.ts
@@ -1,37 +1,35 @@
-import { createTRPCProxyClient, httpBatchLink, } from '@trpc/client';
-import type { DataSourceRouter } from '@shared/api/src/api/data-source';
-import superjson from 'superjson';
-
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import type { DataSourceRouter } from "@shared/api/src/api/data-source";
+import superjson from "superjson";
 
 function getCookie(name: string): string | undefined {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) {
-        return parts.pop()?.split(';').shift();
-    }
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    return parts.pop()?.split(";").shift();
+  }
 }
 
 const getApiRoot = () => {
-    const apiRoot = process.env.NODE_ENV === 'production' ? 'https://atenea-mvp.vercel.app' : 'http://localhost:3000';
-    return apiRoot;
-}
+  const apiRoot =
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_APP_URL
+      : "http://localhost:3000";
+  return apiRoot;
+};
 export const sourceFetcher = createTRPCProxyClient<DataSourceRouter>({
-    links: [
-        httpBatchLink({
-            //TODO: change this to the real url in the env
-            url: `${getApiRoot()}/api/data-source`,
-            fetch(url, options) {
-                return fetch(url, {
-                    ...options,
-                    credentials: "include",
+  links: [
+    httpBatchLink({
+      //TODO: change this to the real url in the env
+      url: `${getApiRoot()}/api/data-source`,
+      fetch(url, options) {
+        return fetch(url, {
+          ...options,
+          credentials: "include",
+        });
+      },
+    }),
+  ],
 
-                })
-            },
-
-        }),
-
-    ],
-
-
-    transformer: superjson,
+  transformer: superjson,
 });
